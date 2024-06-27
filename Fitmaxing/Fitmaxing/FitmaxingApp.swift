036bd8e62f7181d8da64e6a49f0910b161cdd980
B.matar4 <bahaa.matar@student.uts.edu.au>
2024-06-28 02:04:09 +1000

    Bahaa Matar - 12SDD Code On ContentView

---
+++ Fitmaxing/Fitmaxing/FitmaxingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitmaxingApp.swift
+//  Fitmaxing
+//
+//  Created by Bahaa Matar on 21/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitmaxingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
